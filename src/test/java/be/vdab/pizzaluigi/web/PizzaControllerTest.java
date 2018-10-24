fc862d4f1346cb694ba905c5e173723882c76d18
Sam Demeulemeester <sam.demeulemeester@vdabcampus.be>
2018-10-24 11:37:25 +0200

    unit tests

---
+++ src/test/java/be/vdab/pizzaluigi/web/PizzaControllerTest.java
@@ -0,0 +1,47 @@
+package be.vdab.pizzaluigi.web;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import be.vdab.pizzaluigi.entities.Pizza;
+
+public class PizzaControllerTest {
+	private PizzaController controller;
+	@Before
+	public void before() {
+		controller = new PizzaController();
+	}
+	@Test
+	public void pizzaWerktSamenMetDeJspPizza() {
+		ModelAndView modelAndView = controller.pizza(1);
+		assertEquals("pizza", modelAndView.getViewName());
+	}
+	@Test
+	public void pizzaGeeftPizzaDoor() {
+		ModelAndView modelAndView = controller.pizza(1);
+		assertTrue(modelAndView.getModel().get("pizza") instanceof Pizza);
+	}
+	@Test
+	public void onbestaandePizza() {
+		ModelAndView modelAndView = controller.pizza(-1);
+		assertFalse(modelAndView.getModel().containsKey("pizza"));
+	}
+	@Test
+	public void pizzaWerktSamenMetDeJspPizzas() {
+		ModelAndView modelAndView = controller.pizzas();
+		assertEquals("pizzas", modelAndView.getViewName());
+	}
+	@Test
+	public void pizzaGeeftPizzasDoor() {
+		ModelAndView modelAndView = controller.pizzas();
+		assertTrue(modelAndView.getModel().get("pizzas") instanceof Map);
+	}
+
+}
