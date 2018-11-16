29bae6066c771d2c3ffb848e126bafa69816db69
Sam <Sam@192.168.0.163>
2018-11-16 13:48:23 +0100

    integration test v e spring bean

--- src/test/java/be/vdab/pizzaluigi/restclients/FixerKoersClientTest.java
+++ src/test/java/be/vdab/pizzaluigi/restclients/FixerKoersClientTest.java
@@ -1,18 +1,22 @@
 package be.vdab.pizzaluigi.restclients;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@Import(FixerKoersClient.class)
+@PropertySource("application.properties")
 public class FixerKoersClientTest {
+	@Autowired
 	private FixerKoersClient client;
-	@Before
-	public void before() {
-		client = new FixerKoersClient();
-	}
 	@Test
 	public void deKoersMoetPositiefZijn() {
 		assertTrue(client.getDollarKoers().compareTo(BigDecimal.ZERO) > 0);
